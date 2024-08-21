001df3534b03320f1f0b6b6eb093dd3a5f490a01
Vivek Kumar <vivek.kumar@lpu.in>
2024-08-21 14:45:13 +0530

    Inheritance Concept is added

--- OOPS.js
+++ OOPS.js
@@ -15,3 +15,29 @@ class Student{
 //create an instance for the class student
 const student = new Student("John Elia", 15);
 student.displayInfo();
+
+//Inheritance 
+class Car{
+    constructor (brand){
+        this.brand = brand;
+    }
+
+    Present(){
+        return "I have a "+this.brand;
+    }
+}
+
+class Model extends Car{
+    constructor(brand, model){
+        super(brand); //call the parent class 'Car'
+        this.model = model;
+    }
+
+    Show(){
+        return this.Present() + ". This  is a "+this.model + " model.";
+    }
+}
+
+const myCar = new Model("Ford","Mustang");
+console.log(myCar.Show());
+

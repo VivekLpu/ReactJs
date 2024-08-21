fb908b3e44ff9ef67bb0da52867b8220e19d26be
Vivek Kumar <vivek.kumar@lpu.in>
2024-08-21 15:27:33 +0530

    Some arrays methods are added

---
+++ Arrays.js
@@ -0,0 +1,30 @@
+//Intializing the array
+const nums = [1,2,3,4,5,6,7,8,9,10];
+
+//filter()
+const evenNumbers = nums.filter(num => num % 2 == 0)
+console.log("Even Numbers: ",evenNumbers);
+
+//creating a new Array from Existing array
+const squareNumbers = evenNumbers.map(num => num ** 2);
+console.log("Squared Numbers: ",squareNumbers);
+
+
+//find()
+const FirstGreater = squareNumbers.find(num => num > 50);
+console.log("Greater than 50: ",FirstGreater);
+
+//concat()
+const additionNumber = [121,152];
+const combinedArray = squareNumbers.concat(additionNumber);
+console.log("Combined array: ",combinedArray);
+
+//copywithin()
+//array.from()
+const str = '7894561236'
+const numArrayFromStr = Array.from(str);
+console.log("Array From String: ",numArrayFromStr);
+
+//array.of()
+const arrayOfNum = Array.of(12,11,23,12);
+console.log("Array of(): ",arrayOfNum);
